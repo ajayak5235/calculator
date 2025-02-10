a5a58a71b088dd3c3fadfacaff1972558c1e7120
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2025-02-11 02:55:52 +0530

    Update App.jsx

--- frontend_/src/App.jsx
+++ frontend_/src/App.jsx
@@ -1,26 +1,58 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+// import { HashRouter as Router, Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Header from "./components/Header";
+// import LoanCalculator from "./components/loanCalculator";
+// import TaxCalculator from "./components/Taxcalculator";
+// import AgeCalculator from "./components/Agecalculator";
+// import CalorieCalculator from "./components/Caloriescalculator";
+// import BMICalculator from "./components/Bmicalculator";
+// import AdsenseCalculator from "./components/AdsencCalculator";
+// import Footer from "./components/Footer";
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Header />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/loan-calculator" element={<LoanCalculator />} />
+//         <Route path="/tax-calculator" element={<TaxCalculator />} />
+//         <Route path="/age-calculator" element={<AgeCalculator />} />
+//         <Route path="/calorie-calculator" element={<CalorieCalculator />} />
+//         <Route path="/bmi-calculator" element={<BMICalculator />} />
+//         <Route path="/adsense-earning" element={<AdsenseCalculator />} />
+//       </Routes>
+//       <Footer />
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+
 import Header from "./components/Header";
 import LoanCalculator from "./components/loanCalculator";
 import TaxCalculator from "./components/Taxcalculator";
 import AgeCalculator from "./components/Agecalculator";
 import CalorieCalculator from "./components/Caloriescalculator";
 import BMICalculator from "./components/Bmicalculator";
-import AdsenseCalculator from "./components/AdsencCalculator";
+import FinancialCalculators from "./components/AdsencCalculator";
 import Footer from "./components/Footer";
-
 const App = () => {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home />} />
         <Route path="/loan-calculator" element={<LoanCalculator />} />
         <Route path="/tax-calculator" element={<TaxCalculator />} />
         <Route path="/age-calculator" element={<AgeCalculator />} />
         <Route path="/calorie-calculator" element={<CalorieCalculator />} />
         <Route path="/bmi-calculator" element={<BMICalculator />} />
-        <Route path="/adsense-earning" element={<AdsenseCalculator />} />
+        <Route path="/adsense-earning" element={<FinancialCalculators />} />
       </Routes>
       <Footer />
     </Router>
@@ -28,3 +60,4 @@ const App = () => {
 };
 
 export default App;
+
